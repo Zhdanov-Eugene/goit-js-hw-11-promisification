f35e18536c14d780b0417abd0bbc7751300fb984
Zhdanov Yevhen <jeka7753191@gmail.com>
2021-05-17 23:34:23 +0300

    adds changes third

--- task-03/index.js
+++ task-03/index.js
@@ -1,4 +1,47 @@
-// Задание 3
+const randomIntegerFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const makeTransaction = (transaction) => {
+    const delay = randomIntegerFromInterval(200, 500);
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const canProcess = Math.random() > 0.3;
+
+            if (canProcess) {
+                resolve(`Transaction ${transaction.id} processed in ${delay}ms`);
+            };
+            reject(`Error processing transaction ${transaction.id}. Please try again later.`);
+        });
+    }, delay);
+}
+
+const logSuccess = message => {
+    console.log(message);
+};
+
+const logError = message => {
+    console.warn(message);
+};
+
+makeTransaction({ id: 70, amount: 150 })
+    .then(logSuccess)
+    .catch(logError);
+
+makeTransaction({ id: 71, amount: 230 })
+    .then(logSuccess)
+    .catch(logError);
+
+makeTransaction({ id: 72, amount: 75 })
+    .then(logSuccess)
+    .catch(logError);
+
+makeTransaction({ id: 73, amount: 100 })
+    .then(logSuccess)
+    .catch(logError);
+
+
+    // Задание 3
 // Перепиши функцию makeTransaction() так, чтобы она не использовала callback - функции onSuccess и onError, а принимала всего один параметр transaction и возвращала промис.
 
 // const randomIntegerFromInterval = (min, max) => {
